4e27ab64d45b25bddf59c0127a44f47a3656517c
Timon Dubreuil <timondubreuil@outlook.com>
2025-05-21 11:32:09 +0200

    front end du chat bien commencée

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,13 +1,15 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <ChatConversation></ChatConversation>
   </q-page>
 </template>
 
-<script setup>
-//
+<script>
+import ChatConversation from 'src/components/ChatConversation.vue';
+
+export default {
+  components:{
+    ChatConversation
+  }
+}
 </script>
